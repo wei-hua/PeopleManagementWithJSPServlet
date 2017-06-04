044e3d2e97c3b7592b0ee4db0fcf4f2df0509219
unknown <>
2017-06-04 22:33:08 +0800

    JNDI

--- src/personManagement/ControllerServlet.java
+++ src/personManagement/ControllerServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,11 +16,11 @@ public class ControllerServlet extends HttpServlet {
 	private Dao personDao;
 	
 	public void init(){
-		String jdbcUrl=getServletContext().getInitParameter("jdbcUrl");
-		String jdbcUsername=getServletContext().getInitParameter("jdbcUsername");
-		String jdbcPassword=getServletContext().getInitParameter("jdbcPassword");
+//		String jdbcUrl=getServletContext().getInitParameter("jdbcUrl");
+//		String jdbcUsername=getServletContext().getInitParameter("jdbcUsername");
+//		String jdbcPassword=getServletContext().getInitParameter("jdbcPassword");
 		
-		personDao=new Dao(jdbcUrl,jdbcUsername,jdbcPassword);
+		personDao=new Dao();
 	}
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -36,19 +37,44 @@ public class ControllerServlet extends HttpServlet {
 					showNewForm(request,response);
 					break;
 				case "/insert":
+				try {
 					insertPerson(request,response);
+				} catch (NamingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 					break;
 				case "/delete":
+				try {
 					deletePerson(request,response);
+				} catch (NamingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 					break;
 				case "/update":
+				try {
 					updatePerson(request,response);
+				} catch (NamingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 					break;
 				case "/edit":
+				try {
 					showEditForm(request,response);
+				} catch (NamingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 					break;
 				default:
+				try {
 					listPerson(request,response);
+				} catch (NamingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 					break;
 			}
 		}catch (SQLException ex) {
@@ -61,7 +87,7 @@ public class ControllerServlet extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 	
-	private void insertPerson(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException{
+	private void insertPerson(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException, NamingException{
 		int id=Integer.parseInt(request.getParameter("id"));
 		String name=request.getParameter("name");
 		String age=request.getParameter("age");
@@ -70,14 +96,14 @@ public class ControllerServlet extends HttpServlet {
 		response.sendRedirect("list");
 	}
 	
-	private void deletePerson(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException{
+	private void deletePerson(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException, NamingException{
 		int id=Integer.parseInt(request.getParameter("id"));
 		Person person=new Person(id);
 		personDao.deletePerson(person);
 		response.sendRedirect("list");
 	}
 	
-	private void updatePerson(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException{
+	private void updatePerson(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException,NamingException{
 		int id=Integer.parseInt(request.getParameter("id"));
 		String name=request.getParameter("name");
 		String age=request.getParameter("age");
@@ -85,14 +111,14 @@ public class ControllerServlet extends HttpServlet {
 		personDao.updatePerson(person);
 		response.sendRedirect("list");
 	}
-	private void showEditForm(HttpServletRequest request,HttpServletResponse response) throws SQLException, ServletException, IOException{
+	private void showEditForm(HttpServletRequest request,HttpServletResponse response) throws SQLException, ServletException, IOException,NamingException{
 		int id=Integer.parseInt(request.getParameter("id"));
 		Person existingPerson=personDao.getPersonById(id);
 		RequestDispatcher dispatcher=request.getRequestDispatcher("PersonForm.jsp");
 		request.setAttribute("person", existingPerson);
 		dispatcher.forward(request, response);
 	}
-	private void listPerson(HttpServletRequest request,HttpServletResponse response) throws SQLException, ServletException, IOException{
+	private void listPerson(HttpServletRequest request,HttpServletResponse response) throws SQLException, ServletException, IOException,NamingException{
 		List<Person> listPerson=personDao.listAllPerson();
 		RequestDispatcher dispatcher=request.getRequestDispatcher("PersonList.jsp");
 		request.setAttribute("listPerson", listPerson);
--- src/personManagement/Dao.java
+++ src/personManagement/Dao.java
@@ -9,30 +9,44 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+
 
 
 public class Dao {
-	private String jdbcUrl;
-	private String jdbcPassword;
-	private String jdbcUsername;
+//	private String jdbcUrl;
+//	private String jdbcPassword;
+//	private String jdbcUsername;
+//	private Connection jdbcConn;
+//	
+//	public Dao(String jdbcUrl, String jdbcPassword, String jdbcUsername) {
+//		super();
+//		this.jdbcUrl = jdbcUrl;
+//		this.jdbcPassword = jdbcPassword;
+//		this.jdbcUsername = jdbcUsername;
+//	}
+//	
+//	protected void connect()throws SQLException{
+//		if(jdbcConn==null || jdbcConn.isClosed()){
+//			try{
+//				Class.forName("com.mysql.jdbc.Driver");
+//			}catch(ClassNotFoundException e){
+//				throw new SQLException(e);
+//			}
+//			jdbcConn=DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword);
+//		}
+//	}
 	private Connection jdbcConn;
+	private final String JNDINAME="java:comp/env/jndi/mysql";
 	
-	public Dao(String jdbcUrl, String jdbcPassword, String jdbcUsername) {
-		super();
-		this.jdbcUrl = jdbcUrl;
-		this.jdbcPassword = jdbcPassword;
-		this.jdbcUsername = jdbcUsername;
-	}
-	
-	protected void connect()throws SQLException{
-		if(jdbcConn==null || jdbcConn.isClosed()){
-			try{
-				Class.forName("com.mysql.jdbc.Driver");
-			}catch(ClassNotFoundException e){
-				throw new SQLException(e);
-			}
-			jdbcConn=DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword);
-		}
+	protected void connect() throws NamingException, SQLException{
+		Context ctx=new InitialContext();
+		DataSource ds=(DataSource)ctx.lookup(JNDINAME);
+		jdbcConn=ds.getConnection();
 	}
 	
 	protected void disconnect() throws SQLException{
@@ -41,7 +55,7 @@ public class Dao {
 		}
 	}
 	
-	public boolean addPerson(Person person)throws SQLException{
+	public boolean addPerson(Person person)throws SQLException, NamingException{
 		String sql="INSERT INTO person(id,name,age) VALUES (?,?,?)";
 		connect();
 		PreparedStatement statement=jdbcConn.prepareStatement(sql);
@@ -56,7 +70,7 @@ public class Dao {
 		return inserted;
 	}
 	
-	public boolean deletePerson(Person person) throws SQLException{
+	public boolean deletePerson(Person person) throws SQLException, NamingException{
 		String sql="DELETE FROM person WHERE id=?";
 		connect();
 		PreparedStatement statement=jdbcConn.prepareStatement(sql);
@@ -69,7 +83,7 @@ public class Dao {
 		return deleted;
 	}
 	
-	public boolean updatePerson(Person person) throws SQLException{
+	public boolean updatePerson(Person person) throws SQLException, NamingException{
 		String sql="UPDATE person SET name=?,age=? where id=?";
 		connect();
 		PreparedStatement statement=jdbcConn.prepareStatement(sql);
@@ -84,33 +98,30 @@ public class Dao {
 		return updated;
 	}
 	
-	public List<Person> listAllPerson() throws SQLException{
+	public List<Person> listAllPerson() throws SQLException, NamingException{
 		List<Person> listPerson=new ArrayList<>();
 		
 		String sql="select * from person";
 		connect();
-
-		Statement statement = jdbcConn.createStatement();
-		
-		ResultSet resultSet=statement.executeQuery(sql);
-		while(resultSet.next()){
-			int id=resultSet.getInt("id");
-			String name=resultSet.getString("name");
-			String age=resultSet.getString("age");
+			Statement statement = jdbcConn.createStatement();
+			ResultSet resultSet=statement.executeQuery(sql);
+			while(resultSet.next()){
+				int id=resultSet.getInt("id");
+				String name=resultSet.getString("name");
+				String age=resultSet.getString("age");
+				
+				Person person=new Person(id,name,age);
+				listPerson.add(person);
+			}
+			resultSet.close();
+			statement.close();
 			
-			Person person=new Person(id,name,age);
-			listPerson.add(person);
-		}
-		
-		resultSet.close();
-		statement.close();
-		
-		disconnect();
-		
-		return listPerson;
+			disconnect();
+			
+			return listPerson;
 	}
 	
-	public Person getPersonById(int id) throws SQLException{
+	public Person getPersonById(int id) throws SQLException, NamingException{
 		Person person=null;
 		String sql="select * from person where id=?";
 		connect();
